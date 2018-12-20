0659869253c008da5a0aa289832e62f2ef7d96e0
songbyeongha <qudgk0006@naver.com>
2018-12-20 17:55:01 +0900

    add combot_1.py

--- test/main.py
+++ test/main.py
@@ -13,14 +13,17 @@ from selenium import webdriver
 
 app = Flask(__name__)
 
-
+slack_token = "xoxb-502761537154-506755285248-YP7HtDg9VYawsWyThWWq8VAT"
+slack_client_id = "502761537154.508541799559"
+slack_client_secret = "60a697c29a99ddccb23c83cd7ebfa365"
+slack_verification = "Q7OIye9WbM1Ptgqgb0Pevu3k"
 sc = SlackClient(slack_token)
 
 pre_ts = 0
 
 
 # 크롤링 함수 구현하기
-def _crawl_naver_keywords(text):
+def _crawl_naver_keywords(text, goods_number=0):
     # 사용자 호출 후 실행되어야 될 것
     # 여기에 함수를 구현해봅시다.
     result = re.sub(r'<@\S+>', '', text)
@@ -29,95 +32,95 @@ def _crawl_naver_keywords(text):
     elif "추천" in result:
         if "cpu" in result:
             url = "https://search.shopping.naver.com/search/all.nhn?query=cpu&cat_id=&frm=NVSHATC"
-            return send_message(url, "cpu 추천")
+            return send_message(url, "cpu 추천",goods_number)
 
         elif "보드" in result:
             url = "https://search.shopping.naver.com/search/all.nhn?query=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9C&cat_id=&frm=NVSHATC"
-            return send_message(url, "메인보드 추천")
+            return send_message(url, "메인보드 추천",goods_number)
 
         elif "그래픽" in result:
             url = "https://search.shopping.naver.com/search/all.nhn?query=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C&cat_id=&frm=NVSHATC"
-            return send_message(url, "그래픽카드 추천")
+            return send_message(url, "그래픽카드 추천",goods_number)
 
         elif "ram" in result:
             url = "https://search.shopping.naver.com/search/all.nhn?query=ram&cat_id=&frm=NVSHATC"
-            return send_message(url, "ram 추천")
+            return send_message(url, "ram 추천",goods_number)
         elif "ssd" in result:
             url = "https://search.shopping.naver.com/search/all.nhn?query=ssd"
-            return send_message(url, "ssd 추천")
+            return send_message(url, "ssd 추천",goods_number)
 
     elif "리뷰" in result:
         if "많은" in result:
             if "cpu" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=cpu&pagingIndex=1&pagingSize=40&viewType=list&sort=review&frm=NVSHATC&query=cpu"
-                return send_message(url, "cpu 리뷰 많은순")
+                return send_message(url, "cpu 리뷰 많은순",goods_number)
 
             elif "보드" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9C&pagingIndex=1&pagingSize=40&viewType=list&sort=review&frm=NVSHATC&query=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9C"
-                return send_message(url, "메인보드 리뷰 많은순")
+                return send_message(url, "메인보드 리뷰 많은순",goods_number)
 
             elif "그래픽" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C&pagingIndex=1&pagingSize=40&viewType=list&sort=review&frm=NVSHATC&query=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C"
-                return send_message(url, "그래픽카드 리뷰 많은순")
+                return send_message(url, "그래픽카드 리뷰 많은순",goods_number)
 
             elif "ram" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=ram&pagingIndex=1&pagingSize=40&viewType=list&sort=review&frm=NVSHTTL&query=ram"
-                return send_message(url, "ram 리뷰 많은순")
+                return send_message(url, "ram 리뷰 많은순",goods_number)
 
             elif "ssd" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?query=ssd"
-                return send_message(url, "ssd 리뷰 많은순")
+                return send_message(url, "ssd 리뷰 많은순",goods_number)
     elif "가격" in result:
         if "낮은" in result:
             if "cpu" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=cpu&pagingIndex=1&pagingSize=40&viewType=list&sort=price_asc&frm=NVSHATC&query=cpu"
-                return send_message(url, "cpu 낮은 가격순")
+                return send_message(url, "cpu 낮은 가격순",goods_number)
 
             elif "보드" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9C&pagingIndex=1&pagingSize=40&viewType=list&sort=price_asc&frm=NVSHATC&query=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9C"
-                return send_message(url, "메인보드 낮은 가격순")
+                return send_message(url, "메인보드 낮은 가격순",goods_number)
 
             elif "그래픽" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C&pagingIndex=1&pagingSize=40&viewType=list&sort=price_asc&frm=NVSHATC&query=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9CC"
-                return send_message(url, "그래픽카드 낮은 가격순")
+                return send_message(url, "그래픽카드 낮은 가격순",goods_number)
 
             elif "ram" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=ram&pagingIndex=1&pagingSize=40&viewType=list&sort=price_asc&frm=NVSHATC&query=ram"
-                return send_message(url, "ram 낮은 가격순")
+                return send_message(url, "ram 낮은 가격순",goods_number)
 
             elif "ssd" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=ssd&pagingIndex=1&pagingSize=40&viewType=list&sort=price_asc&frm=NVSHTTL&query=ssd"
-                return send_message(url, "ssd 낮은 가격순")
+                return send_message(url, "ssd 낮은 가격순",goods_number)
 
         elif "높은" in result:
             if "cpu" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=cpu&pagingIndex=1&pagingSize=40&viewType=list&sort=price_dsc&frm=NVSHATC&query=cpu"
-                return send_message(url, "cpu 높은 가격순")
+                return send_message(url, "cpu 높은 가격순",goods_number)
 
             elif "보드" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9C&pagingIndex=1&pagingSize=40&viewType=list&sort=price_dsc&frm=NVSHATC&query=%EB%A9%94%EC%9D%B8%EB%B3%B4%EB%93%9CC"
-                return send_message(url, "메인보드 높은 가격")
+                return send_message(url, "메인보드 높은 가격",goods_number)
 
             elif "그래픽" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C&pagingIndex=1&pagingSize=40&viewType=list&sort=price_dsc&frm=NVSHATC&query=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C"
-                return send_message(url, "그래픽카드 높은 가격")
+                return send_message(url, "그래픽카드 높은 가격",goods_number)
 
             elif "ram" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=ram&pagingIndex=1&pagingSize=40&viewType=list&sort=price_dsc&frm=NVSHATC&query=ram"
-                return send_message(url, "ram 높은 가격")
+                return send_message(url, "ram 높은 가격",goods_number)
 
             elif "ssd" in result:
                 url = "https://search.shopping.naver.com/search/all.nhn?origQuery=ssd&pagingIndex=1&pagingSize=40&viewType=list&sort=price_dsc&frm=NVSHTTL&query=ssd"
-                return send_message(url, "ssd 높은 가격")
+                return send_message(url, "ssd 높은 가격",goods_number)
     elif "안녕" in result:
         return u"안녕하세요!! 컴퓨터 부품을 알려드립니다.^^"
     else:
         return u"명령어가 없어요!!"
 
-def _search_event(text):
+def _search_event(text,num=0):
     result = re.sub(r'<@\S+>', '', text)
     if "검색 :" in result:
-        return search_def(result)
+        return search_def(result,int(num))
 
 # 이벤트 핸들하는 함수
 def _event_handler(event_type, slack_event):
@@ -127,14 +130,23 @@ def _event_handler(event_type, slack_event):
         channel = slack_event["event"]["channel"]
         text = slack_event["event"]["text"]
 
-        searchs =  _search_event(text)
-        keywords = _crawl_naver_keywords(text)
-        sc.api_call(
-            "chat.postMessage",
-            channel=channel,
-            text=keywords,
-            attachments=json.dumps([searchs])
-        )
+        if "검색 :" in text:
+            searchs = _search_event(text,0)
+            sc.api_call(
+                "chat.postMessage",
+                channel=channel,
+                attachments=json.dumps([searchs])
+            )
+        else:
+            keywords = _crawl_naver_keywords(text)
+
+            sc.api_call(
+                "chat.postMessage",
+                channel=channel,
+                text=keywords,
+                # attachments=json.dumps([searchs])
+            )
+
 
         return make_response("App mention message has been sent", 200, )
 
--- test/recommend.py
+++ test/recommend.py
@@ -11,26 +11,35 @@ from slackclient import SlackClient
 from flask import Flask, request, make_response, render_template
 
 
-def send_message(url, title):
+def send_message(url, title, goods_number = 0):
     source = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(source, "html.parser")
     keywords = []
     titles = []
     title_url = []
+    img_url = []
     for tit in soup.find_all("div", class_="info"):
         titles.append(tit.find("a", class_="tit"))
         title_url.append(tit.find("a", class_="tit")["href"])
     prices = soup.find_all("span", class_="num _price_reload")
     # 추가 details
     details = soup.find_all("span", class_="detail")
+    # 추가 img_url
+    for img in soup.find_all("div", class_="img_area"):
+        img_url.append(img.find("img")["src"])
+
     keywords.append(title + " Top 5\n\n")
 
     for i in range(5):
         keywords.append(str(i + 1) + "위 -> \n 상품명 : <"+str(title_url[i])+"|" + titles[i].get_text().strip() + ">\n 가격 : " + prices[i].get_text().strip() + "원\n 상품 상세 : \n     " + details[i].get_text().strip().replace("|", ",\n     ").replace(":", "->") + "\n    ")
+        keywords.append(str(img_url[i]))
 
     return u'\n'.join(keywords)
 
-def search_def(key):
+
+
+
+def search_def(key, goods_num=0):
     driver = webdriver.Chrome(r"C:\Users\student\Desktop\chromedriver_win32\chromedriver.exe")
     # 사용자 호출 전에 실행되어야 될 것
     driver.get("https://search.shopping.naver.com/search/category.nhn?cat_id=50000097")
@@ -46,16 +55,17 @@ def search_def(key):
     img_url = []
     for tit in soup.find_all("div", class_="info"):
         titles.append(tit.find("a", class_="tit"))
-        title_url.append(tit.find("a", class_="tit")["href"])
+        # title_url.append(tit.find("a", class_="tit")["href"])
+        title_url.append(tit.find("a")["href"])
     prices = soup.find_all("span", class_="num _price_reload")
     # time.sleep(2)
     for img in soup.find_all("div", class_="img_area"):
         img_url.append(img.find("img")["src"])
     # 추가 details
     details = soup.find_all("span", class_="detail")
-    keywords = "검색 결과1 -> \n상품명 : <" + str(title_url[0]) + "|" + titles[0].get_text().strip() + ">\n 가격 : " + prices[0].get_text().strip() + "원\n 상품 상세 : \n" + details[0].get_text().replace("|", ",").replace(":", "->").strip() + "\n"
+    keywords = "검색 결과 -> \n상품명 : <" + str(title_url[goods_num]) + "|" + titles[goods_num].get_text().strip() + ">\n 가격 : " + prices[goods_num].get_text().strip() + "원\n 상품 상세 : \n" + details[goods_num].get_text().replace("|", ",").replace(":", "->").strip() + "\n"
     driver.close()
     msg = {}
     msg["text"] = keywords
-    msg["image_url"] = img_url[0]
+    msg["image_url"] = img_url[goods_num]
     return msg
